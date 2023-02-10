899d0c5bbb0addcff9dad056502c1695a8f6bace
shantanubose01 <sbose1918@gmail.com>
2023-02-10 17:39:34 +0530

    first commit

---
+++ Models/models.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const dataschema = new mongoose.Schema({
+    'adult': Boolean,
+    "genre_ids": Array,
+    "id": Number,
+    "original_language": String,
+    "original_title": String,
+    "overview": String,
+    "popularity": Number,
+    "poster_path": String,
+    "release_date": String,
+    "title": String,
+    "video": Boolean,
+    "vote_average": Number,
+    "vote_count": Number
+})
+const userschema = new mongoose.Schema({
+    'googleId' : String,
+    'username' : String
+})
+// userschema.statics.findOrCreate = function findOrCreate(profile, cb){
+//     var userObj = new this();
+//     this.findOne({googleId : profile.id},function(err,result){ 
+//         if(!result){
+//             userObj.googleId = profile.id ;
+//             userObj.username = profile.displayName;
+//             //....
+//             userObj.save(cb);
+//         }else{
+//             cb(err,result);
+//         }
+//     });
+// };
+const User = mongoose.model('user', userschema);
+
+const newrelease = mongoose.model('newrelease', dataschema);
+const blockbuster = mongoose.model('blockbuster', dataschema);
+const hollywood = mongoose.model('hollywood', dataschema);
+const popular = mongoose.model('popular', dataschema);
+const telugu = mongoose.model('telugu', dataschema);
+const tamil = mongoose.model('tamil', dataschema);
+const korean = mongoose.model('korean', dataschema);
+const tamiltv = mongoose.model('tamiltv', dataschema);
+const prabhas = mongoose.model('prabhas', dataschema);
+
+module.exports = { newrelease, blockbuster, hollywood, popular, telugu, tamil, korean, tamiltv, prabhas , User};
